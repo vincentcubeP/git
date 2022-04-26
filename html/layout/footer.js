7403c114ed938b3e4a563bcda24a83f78b932b43
VCS <vincentcubeP@gmail.com>
2022-04-26 09:35:46 +0900

    commit

---
+++ html/layout/footer.js
@@ -0,0 +1,8 @@
+var str =
+`<div class="footer">
+    <p>Copyright @ VCS. All rights reserved.</p>
+    <p>vincentcubeP@gmail.com</p>
+</div>
+`;
+
+document.write(str);
---
+++ html/layout/header.js
@@ -0,0 +1,54 @@
+var str =
+`<div class="header">
+    <h1>
+        <a href="./index.html"><img src="./img/vcs-logo-white.png" alt=""></a>
+    </h1>
+    <input class="burger-check" type="checkbox" id="burger-check" />
+    <nav class="nav">
+      <ul class="menu">
+        <li>
+          <a her="">3x3x3</a>
+          <ul class="menu-sub">
+            <li><a href="../../html/rubiks/F2L.html">F2L</a></li>
+            <li><a href="../../html/rubiks/OLL.html">OLL</a></li>
+            <li><a href="../../html/rubiks/PLL.html">PLL</a></li>
+          </ul>
+        </li>
+        <li>
+          <a her="">2x2x2</a>
+          <ul class="menu-sub">
+            <li><a href="">Ortega</a></li>
+            <li><a href="">EG</a></li>
+          </ul>
+        </li>
+        <li>
+          <a her="">Big cube</a>
+          <ul class="menu-sub">
+            <li><a href="">4x4x4</a></li>
+            <li><a href="">5x5x5</a></li>
+            <li><a href="">6x6x6</a></li>
+            <li><a href="">7x7x7</a></li>
+          </ul>
+        </li>
+        <li>
+          <a her="">Megaminx</a>
+          <ul class="menu-sub">
+            <li><a href="">2look</a></li>
+            <li><a href="">OLL</a></li>
+            <li><a href="">PLL</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>    
+    <div>
+    </div>    
+    <label class="burger-icon" for="burger-check">
+      <span class="burger-sticks"></span>
+    </label>
+    <div class="menu-burger">
+      <div style="width: 200px;"></div>
+    </div>
+</div>
+`;
+
+document.write(str);
